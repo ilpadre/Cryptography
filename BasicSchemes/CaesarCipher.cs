63f74980e77dee25af4e942ec16ae68ced02fc35
ilpadre <ilpadre1953@gmail.com>
2018-04-25 21:02:09 -0400

    Initial commit

---
+++ BasicSchemes/CaesarCipher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BasicSchemes
+{
+    public class CaesarCipher
+    {
+        private string alphabet = $"abcdefghijklmnopqrstuvwxyz";
+        private int shift = 3;
+        public string Encrypt(string plainText)
+        {
+            var charArray = plainText.ToLower().ToCharArray();
+            string cipherText = string.Empty;
+            for (int i = 0; i <= charArray.Length - 1; i++)
+            {
+                var ch = charArray[i];
+                var index = alphabet.IndexOf(ch);
+                if (index >= 0)
+                {
+                    index = (index + shift) % alphabet.Length;
+                    cipherText += alphabet[index];
+                }
+                else
+                {
+                    cipherText += ' ';
+                }
+
+            }
+            
+            return cipherText;
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            var charArray = cipherText.ToLower().ToCharArray();
+            string plainText = string.Empty;
+            for (int i = 0; i <= charArray.Length - 1; i++)
+            {
+                var ch = charArray[i];
+                var index = alphabet.IndexOf(ch);
+                if (index >= 0)
+                {
+                    index = (index - shift) % alphabet.Length;
+                    plainText += alphabet[index];
+                }
+                else
+                {
+                    plainText += ' ';
+                }
+
+            }
+            return plainText;
+        }
+    }
+}
---
+++ BasicSchemes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BasicSchemes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CaesarCipher cc = new CaesarCipher();
+            var plainText = "How now brown cow";
+            var cipherText = cc.Encrypt(plainText);
+            var newPlainText = cc.Decrypt(cipherText);
+
+            Console.WriteLine($"Message text = {plainText}");
+            Console.WriteLine($"Encrypted text = {cipherText}");
+            Console.WriteLine($"Decrypted text = {newPlainText}");
+
+            Console.ReadLine();
+        }
+    }
+}
